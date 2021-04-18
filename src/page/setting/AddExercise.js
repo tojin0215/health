cd2d0d08c97a7d18b999c26e8c1685494301bb38
kkyubrother <kkyubrother@naver.com>
2021-04-19 06:03:31 +0900

    운동 검색 및 추가 기능 구현

--- src/page/setting/AddExercise.js
+++ src/page/setting/AddExercise.js
@@ -10,8 +10,10 @@ import Dropdown from 'react-dropdown';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import '../../styles/exercise/Exercise.css';
 
-const ip = '13.124.141.28';
-const List = [
+// const ip = '13.124.141.28';
+const ip = '127.0.0.1';
+
+const List2 = [
     {'no':1,'name':'AAA','tool':'바벨','aa':'상체','set':'3','bb':'10','cc':'10분','link':'www.www.www'},
     {'no':2,'name':'BBB','tool':'바벨','aa':'하체','set':'3','bb':'10','cc':'10분','link':'www.www.www'},
     {'no':3,'name':'CCC','tool':'바벨','aa':'전신','set':'3','bb':'10','cc':'10분','link':'www.www.www'},
@@ -28,6 +30,7 @@ const List = [
     {'no':14,'name':'BBB','tool':'바벨','aa':'하체','set':'3','bb':'10','cc':'10분','link':'www.www.www'},
     {'no':15,'name':'CCC','tool':'바벨','aa':'전신','set':'3','bb':'10','cc':'10분','link':'www.www.www'},
 ]
+const List = []
 
 
 require('moment-timezone');
@@ -44,8 +47,22 @@ class AddExercise extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            exerciseList: List,
+            exerciseListLoaded: false,
+
             search:"",
             item:options[0],
+
+            fitness_no: this.props.userinfo.fitness_no,
+            name: "",
+            part: 0,
+            machine: "",
+            url: "http://localhost",
+            default_data_type: -1,
+            default_data: "",
+            default_rest_second: -1,
+            default_set_count: -1,
+            is_default: false,
         };
         this.handleOnClick = this.handleOnClick.bind(this);
         
@@ -62,10 +79,38 @@ class AddExercise extends Component {
         });
     }
 
-    handleChange = (e) => { 
-        this.setState({ 
-            [e.target.id]: e.target.value,
-        }); 
+    handleChange = (e) => {
+        let v;
+        if (e.target.name === "part") {
+            if (e.target.checked) {
+                this.setState({[e.target.name]: JSON.parse(JSON.stringify({data: Number(this.state.part)}))['data'] + Number(e.target.value)});
+            } else {
+                this.setState({[e.target.name]: JSON.parse(JSON.stringify({data: Number(this.state.part)}))['data'] - Number(e.target.value)});
+            }
+        } else if (e.target.name==="default_set_count") {
+            if (/^\d+$/.test(e.target.value)) {this.setState({[e.target.name]: Number(e.target.value)});}
+            else {
+                if (e.target.value==="") {this.setState({[e.target.name]: ""}); return;}
+                alert(e.target.value + "는 숫자가 아닙니다.");
+                e.target.value = "";
+            }
+        } else if (e.target.name==="default_data") {
+            if (/^\d+$/.test(e.target.value)) {this.setState({[e.target.name]: Number(e.target.value)});}
+            else {
+                if (e.target.value==="") {this.setState({[e.target.name]: ""}); return;}
+                alert(e.target.value + "는 숫자가 아닙니다.");
+                e.target.value = "";
+            }
+        } else if (e.target.name==="default_rest_second") {
+            if (/^\d+$/.test(e.target.value)) {this.setState({[e.target.name]: Number(e.target.value)});}
+            else {
+                if (e.target.value==="") {this.setState({[e.target.name]: ""}); return;}
+                alert(e.target.value + "는 숫자가 아닙니다.");
+                e.target.value = "";
+            }
+        } else {
+            this.setState({[e.target.name]: e.target.value}); 
+        }
     };
     
     search = () =>{
@@ -79,22 +124,53 @@ class AddExercise extends Component {
         }
         //customer 참고해서 검색기능 넣기
 
-        // fetch("http://"+ip+":3001/customer?type=search"+it+"&search="+this.state.search+"&fn="+this.props.userinfo.fitness_no, {
-        //     method: "GET",
-        //     headers: {
-        //       'Content-type': 'application/json'
-        //   }
-        // })
-        // .then(response => response.json())
-        // .then(res => {
-        //         // let arr = [];
-        //         // for(let i=(res.length-1) ; i>=0 ; i--){
-        //         //     let sor = res[i].solar_or_lunar===true?"양":"음";
-        //         //     let s = res[i].sex===true?"남":"여";
-        //         //     arr.push({"no":res[i].member_no, "name":res[i].name, "sex":s, "phone":res[i].phone, "in_charge":res[i].in_charge,"start_date":moment(res[i].start_date).format("YYYY/MM/DD")+"~ ("+res[i].period+"개월)", "resi_no":res[i].resi_no+ " ("+sor+")" })
-        //         // }
-        //         // this.setState({customerList : arr});
-        //     });
+        let search = this.state.search;
+        if (it==='2') {
+            let v = 0;
+            if (/상체/.test(this.state.search)){search = search.replace("상체", ""); v = v + 1}
+            if (/하체/.test(this.state.search)){search = search.replace("하체", ""); v = v + 2}
+            if (/전신/.test(this.state.search)){search = search.replace("전신", ""); v = v + 4}
+            if (/코어/.test(this.state.search)){search = search.replace("코어", ""); v = v + 8}
+            if (/유산소/.test(this.state.search)){search = search.replace("유산소", ""); v = v + 16}
+
+            if (v===0) {alert("부위를 입력바랍니다."); return;}
+            search = v
+        }
+
+        fetch("http://"+ip+":3001/exercise?type=search"+it+"&search="+search+"&fn="+this.props.userinfo.fitness_no, {
+            method: "GET",
+            headers: {
+              'Content-type': 'application/json'
+          }
+        })
+        .then(response => response.json())
+        .then(res => {
+            console.log(res);
+                let arr = [];
+                for(let i=(res.length-1) ; i>=0 ; i--){
+                    let part = ", ";
+                    let part_num = Number(res[i].part);
+                    console.log(part_num);
+                    if (part_num >= 16) {part = "유산소, " + part; part_num = part_num - 16}
+                    if (part_num >= 8) {part = "코어, " + part; part_num = part_num - 8}
+                    if (part_num >= 4) {part = "전신, " + part; part_num = part_num - 4}
+                    if (part_num >= 2) {part = "하체, " + part; part_num = part_num - 2}
+                    if (part_num === 1) {part = "상체, " + part;}
+                    part = part.slice(0, -2)
+
+                    arr.push({
+                        "no": res[i].exercise_no,
+                        "name": res[i].name,
+                        "tool": res[i].machine,
+                        "aa": part,
+                        "set": res[i].default_set_count,
+                        "bb": res[i].default_data,
+                        "cc": res[i].default_rest_second,
+                        "link": res[i].url,
+                    })
+                }
+                this.setState({exerciseList : arr});
+            });
     }
     selectItem = (e) =>{
         if(e.value == "이름"){
@@ -107,10 +183,73 @@ class AddExercise extends Component {
         }
     }
 
+    AddExercise = () => {
+        
+        if (this.state.name==="") {alert("이름이 없습니다"); return;}
+        else if (this.state.part===0) {alert("운동 부위가 선택되지 않았습니다"); return;}
+        else if (this.state.machine==="") {alert("운동 기구가 없습니다"); return;}
+        else if (this.state.url==="") this.setState({url: ""});
+        else if (this.state.default_data_type==="") this.setState({default_data_type: 1});
+        else if (this.state.default_data==="") {alert("운동 기본 횟수가 없습니다"); return;}
+        else if (this.state.default_rest_second==="") {alert("쉬는 시간이 없습니다"); return;}
+        else if (this.state.default_set_count==="") {alert("운동 세트 횟수가 없습니다"); return;}
+        else if (this.state.is_default===undefined) this.setState({is_default: false});
+        else {
+        // if (this.state.name==="") this.setState({name: ""});
+        // else if (this.state.part == false) this.setState({part: ""});
+        // else if (this.state.machine == false) this.setState({machine: ""});
+        // else if (this.state.url == false) this.setState({url: ""});
+        // else if (this.state.default_data_type==false) this.setState({default_data_type: ""});
+        // else if (this.state.default_data==false) this.setState({default_data: ""});
+        // else if (this.state.default_rest_second==false) this.setState({default_rest_second: ""});
+        // else if (this.state.default_set_count==false) this.setState({default_set_count: ""});
+        // else if (this.state.is_default === undefined) this.setState({is_default: false});
+        // else {
+            const userinfo = {member_no: -1, manager_name: "", fitness_no: -1, fitness_name: ""}
+            if (this.state.userinfo!==undefined) {
+                userinfo['member_no'] = this.state.userinfo.member_no;
+                userinfo['manager_name'] = this.state.userinfo.manager_name;
+                userinfo['fitness_no'] = this.state.userinfo.fitness_no;
+                userinfo['fitness_name'] = this.state.userinfo.fitness_name;
+            }
+
+            fetch('http://'+ip+":3001/exercise", {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    fitness_no: userinfo.fitness_no,
+
+                    name: this.state.name,                                  // 운동 이름
+                    part: this.state.part,          // 운동 부위
+                    machine: this.state.machine,    // 운동 기구
+                    url: this.state.url,            // 운동 링크
+                    default_data_type: this.state.default_data_type,        // 기본 운동 데이터 타입
+                    default_data: this.state.default_data,                  // 기본 값
+                    default_rest_second: this.state.default_rest_second,    // 쉬는 시간(초)
+                    default_set_count: this.state.default_set_count,        // 세트 횟수
+                    is_default: this.state.is_default,  // 기본값인가?
+                })
+            })
+            .then(res => {res.json()})
+            .then(res => {alert('운동 등록됨');})
+            .catch(err => {
+                alert(err);
+            });
+        }
+    }
+    handleKeyUp = (e) => {
+        if (e.key === "Enter") {
+            console.log(e);
+            this.AddExercise();
+          }
+    }
+
     render() {
         const { userinfo } = this.props;
-        console.log("userinfo : ");
-        console.log(userinfo); //나중에 DB에서 불러올 때 사용, 로그인된 ID, fitness 정보 들어있음
+        // console.log("userinfo : ");
+        // console.log(userinfo); //나중에 DB에서 불러올 때 사용, 로그인된 ID, fitness 정보 들어있음
         
         const options1 = {
             noDataText: '등록된 운동이 없습니다.',
@@ -118,6 +257,7 @@ class AddExercise extends Component {
             hideSizePerPage:true,
             sizePerPage:5
         };
+        if (!this.state.exerciseListLoaded) {this.setState({exerciseListLoaded: true});this.search();}
 
         return (
             <div>
@@ -137,47 +277,47 @@ class AddExercise extends Component {
                 <form style={{flexDirection:'column'}}>
                 <div className="input-row">
                                 <label className="label-description">운동 이름</label>
-                                <input id="name" placeholder="name" onChange={this.handleChange} />
+                                <input id="name" placeholder="name" name="name" onChange={this.handleChange} />
                             </div>
                             
                             <div className="input-row">
                                 <label className="label-description">운동 기구</label>
-                                <input id="tool" placeholder="machine" onChange={this.handleChange} />
+                                <input id="tool" placeholder="machine" name="machine" onChange={this.handleChange} />
                             </div>
                             
                             <div className="input-row">
                                 <label className="label-description">부위</label>
                                 <div className="part">
-                                   <label><input type="checkBox" value='1' onChange={this.handleChange}/>상체</label>
-                                   <label><input type="checkBox" value='2' onChange={this.handleChange}/>하체</label>
-                                   <label><input type="checkBox" value='3' onChange={this.handleChange}/>전신</label>
-                                   <label><input type="checkBox" value='4' onChange={this.handleChange}/>코어</label>
-                                   <label><input type="checkBox" value='5' onChange={this.handleChange}/>유산소</label>
+                                   <label><input type="checkBox" value='1' name="part" onChange={this.handleChange}/>상체</label>
+                                   <label><input type="checkBox" value='2' name="part" onChange={this.handleChange}/>하체</label>
+                                   <label><input type="checkBox" value='4' name="part" onChange={this.handleChange}/>전신</label>
+                                   <label><input type="checkBox" value='8' name="part" onChange={this.handleChange}/>코어</label>
+                                   <label><input type="checkBox" value='16' name="part" onChange={this.handleChange}/>유산소</label>
                                 </div>
                             </div>
 
                             <div className="input-row">
                                 <label>세트</label>
-                                <input id="세트" placeholder="machine" onChange={this.handleChange}/>
+                                <input id="세트" placeholder="세트" name="default_set_count" onChange={this.handleChange}/>
                                 <label>횟수</label>
-                                <input id="횟수" placeholder="machine" onChange={this.handleChange}/>
+                                <input id="횟수" placeholder="횟수" name="default_data" onChange={this.handleChange}/>
                                 <label>휴식시간</label>
-                                <input id="휴식시간" placeholder="machine" onChange={this.handleChange}/>
+                                <input id="휴식시간" placeholder="휴식시간" name="default_rest_second" onChange={this.handleChange}/>
                             </div>
                             <div className="input-row">
                                 <label className="label-description">영상 링크</label>
-                                <input id="link" placeholder="link" onChange={this.handleChange} />
+                                <input id="link" placeholder="link" name="url" onChange={this.handleChange} onKeyUp={this.handleKeyUp} />
                             </div>
                             
-                            <button type="button" onClick={this.handleOnClick}>저장하기</button>
+                            <button type="button" onClick={this.AddExercise}>저장하기</button>
                 </form>
                 <br></br>
                 <div className='customerSearch'>
                         <Dropdown className='searchDrop' options={options} onChange={this.selectItem} value={this.state.item} placeholder="Select an option" />
-                        <input type="text" id='search' checked={this.state.search} onChange={this.handleChange} />
+                        <input type="text" id='search' name="search" checked={this.state.search} onChange={this.handleChange} />
                         <button type="button" onClick={this.search}> 운동 검색 </button>
                     </div>
-                <BootstrapTable data={ List } hover 
+                <BootstrapTable data={ this.state.exerciseList } hover 
                     pagination={ List.length > 1 }
                     options={options1}
                     tableHeaderClass='tableHeader'  
