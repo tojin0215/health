59626f1c063b0caaa27d16a8bf271bf1a451c645
k <a@example.com>
2021-04-26 23:59:12 +0900

    'session1'

--- src/page/exercise/AssignCheckExercise.js
+++ src/page/exercise/AssignCheckExercise.js
@@ -70,6 +70,51 @@ class AssignCheckExercise extends Component {
         this.props.history.push('/');
     };
 
+    componentDidMount() { //컴포넌트 렌더링이 맨 처음 완료된 이후에 바로 세션확인
+        // get cookie by name
+        function getCookie(name) {
+            var value = "; " + document.cookie; 
+            var parts = value.split("; " + name + "="); 
+            if (parts.length == 2) return parts.pop().split(";").shift();
+        }
+   
+        // get loginData from cookie
+        let loginData = getCookie('key');
+        // if loginData is undefined, do nothing
+        if(typeof loginData === "undefined"){
+            this.props.history.push('/');
+            return;
+        } 
+   
+        // decode base64 & parse json
+        loginData = JSON.parse(atob(loginData));
+        // if not logged in, do nothing
+        if(!loginData.isLoggedIn){
+            this.props.history.push('/');
+            return;
+        } 
+   
+        // page refreshed & has a session in cookie,
+        // check whether this cookie is valid or not
+        this.props.getStatusRequest().then(
+            () => {
+                // if session is not valid
+                if(!this.props.status.valid) {
+                    // logout the session
+                    loginData = {
+                        isLoggedIn: false,
+                        id: ''
+                    };
+   
+                    document.cookie='key=' + btoa(JSON.stringify(loginData));
+   
+                    // and notify
+                    alert("Your session is expired, please log in again")
+                }
+            }
+        );
+    }
+
     sendAssign2 = (arr, last_group_no) => {
         let url = 'http://' + ip + '/assignexercise';
         arr.forEach((ex) => {
@@ -427,12 +472,21 @@ class AssignCheckExercise extends Component {
 const AssignCheckExerciseStateToProps = (state) => {
     return {
         userinfo: state.authentication.userinfo,
+        status: state.authentication.status
+    };
+};
+
+const AssignCheckExerciseDispatchToProps = (dispatch) => {
+    return {
+        getStatusRequest: () => {
+            return dispatch(getStatusRequest());
+        },
     };
 };
 
 export default connect(
     AssignCheckExerciseStateToProps,
-    undefined
+    AssignCheckExerciseDispatchToProps
 )(AssignCheckExercise);
 //새 page 추가 시 guide : 이 폴더 안에 페이지 하나 더 만든 후, src/component/app.js && src/page/index 함께 변경해주세요
 //잘 모르겠으면 customer폴더 참고
