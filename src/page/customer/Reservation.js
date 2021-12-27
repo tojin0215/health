3475bea0d613be9b9b2d5fe083235a7a7dcda886
kkyubrother@naver.com <Lun@ria1024E420>
2021-12-27 18:05:40 +0900

    예약 페이지 일부 작업

--- src/page/customer/Reservation.js
+++ src/page/customer/Reservation.js
@@ -1,43 +1,154 @@
 import React, { Component, useState } from 'react';
+import { render } from 'react-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import Dropdown from 'react-dropdown';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 import Navigation from '../../component/navigation/Navigation';
 import Header from '../../component/header/Header';
 import Footer from '../../component/footer/Footer';
-import { connect } from 'react-redux';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
 import MegaMenu from '../../component/navigation/Menu';
-import { SERVER_URL } from '../../const/settings';
-const ip = SERVER_URL;
+import moment from 'moment';
+import axios from 'axios';
+
+
+const getSearchUser = (type, search, fitness_no) => (
+    axios.get("/api/customer", {params: {
+        type: type,
+        search: search,
+        fn: fitness_no,
+    }}).then(response => response.data)
+)
+
+
+const CustomerFindAndSelect = (show, setShow, fitness_no, setCustomer) => {
+    const search_type_options = ['이름', '핸드폰'];
+    const [search_type, setSearchType] = useState(search_type_options[0])
+    const [search, setSearch] = useState("")
+    const [customers, setCustomers] = useState([]);
+
+    const handleSearch = () => {
+        let type = '0';
+		if (search_type === '이름') {
+			type = '0';
+		} else if (search_type === '핸드폰') {
+			type = '1';
+		}
+        getSearchUser(type, search, fitness_no)
+        .then(result => {
+            setCustomers(result.map(value => {
+                return {
+                    no: value.member_no,
+                    member_no: value.member_no,
+                    userName: value.name,
+                    phone: value.phone,
+                }
+            }))
+        });
+    }
+    
+    return (<Dialog
+    open={show}
+    onClose={() => setShow(false)}
+    maxWidth='lg'>
+        <DialogTitle>고객 검색</DialogTitle>
+        <DialogContent>
+            <div className='customerSearch'>
+                <Dropdown
+                    className='searchDrop'
+                    options={search_type_options}
+                    onChange={e => setSearchType(e.value)}
+                    value={search_type}
+                />
+                {/*.searchDrop */}
+                <input
+                    type='text'
+                    id='search'
+                    checked={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
+                {/*#search */}
+                <button type='button' onClick={handleSearch}>
+                    고객 검색
+                </button>
+            </div>
+            {/*.customerSearch */}
+            <Table className='addsalesSearchTable'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>번호</TableCell>
+                        <TableCell>이름</TableCell>
+                        <TableCell>폰번호</TableCell>
+                        <TableCell>선택</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {customers ? (
+                        customers.map((c) => (
+                            <TableRow>
+                                <TableCell>{c.no}</TableCell>
+                                <TableCell>{c.userName}</TableCell>
+                                <TableCell>{c.phone}</TableCell>
+                                <TableCell>
+                                    <DialogActions>
+                                        <button
+                                            type='button'
+                                            onClick={() => setCustomer(c)}
+                                            id={c.no}
+                                            value={[c.userName, c.phone]}
+                                        >
+                                            선택
+                                        </button>
+                                        {/*#{c.no} */}
+                                    </DialogActions>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan='6' align='center'></TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </DialogContent>
+        <DialogActions>
+            <button type='button' onClick={() => setShow(false)}>
+                닫기
+            </button>
+        </DialogActions>
+    </Dialog>)
+}
+
 
 class Reservation extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            reservation: [],
+            show: false,
+            fitness_no: 1,
+            customer: null,
+
+            customer_name: "홍길동",
+            reservation_date: moment(),
+            reservation_time: moment(),
+            exercise_name: "PT기구",
+            is_cancel: false,
+            cancel_comment: "",
         }
-        this.reservationSelect();
     }
-
-    goLogin = () => {
-        this.props.history.push('/');
-    };
-
-    reservationSelect = () => {
-        fetch(ip + '/reservation/select',
-            {
-                method: 'GET',
-                headers: {
-                    'Content-type': 'application/json',
-                }
-            })
-            .then((result) => result.json())
-            .then((result) => {
-                this.setState({ reservation: result })
-                console.log(result)
-            })
-    }
-
     render() {
         return (
             <div className='addCustomer'>
@@ -45,13 +156,21 @@ class Reservation extends Component {
                     <Header />
                     <Navigation goLogin={this.goLogin} />
                     <MegaMenu />
-                    <div className='container'>
-                        Reservation
-                    </div>
                 </header>
-                <div className='footer'>
-                    <Footer />
-                </div>
+                <CustomerFindAndSelect
+                show={this.state.show}
+                setShow={value => this.setState({show: value})}
+                fitness_no={this.state.fitness_no}
+                setCustomer={c => this.setState({customer: c})}
+                />
+                <Container>
+                    <Row><Col>회원이름:</Col><Col>{this.state.customer_name}</Col></Row>
+                    <Row><Col>날짜:</Col><Col>{this.state.reservation_date.format("YYYY-MM-DD")}</Col></Row>
+                    <Row><Col>시간:</Col><Col>{this.state.reservation_time.format("hh:mm")}</Col></Row>
+                    <Row><Col>운동이름:</Col><Col>{this.state.exercise_name}</Col></Row>
+                    <Row><Col>취소유무:</Col><Col>{this.state.is_cancel? "취소됨" : "예약됨"}</Col></Row>
+                    <Row><Col>취소사유:</Col><Col>{this.state.is_cancel && this.state.cancel_comment}</Col></Row>
+                </Container>
             </div>
         )
     }
