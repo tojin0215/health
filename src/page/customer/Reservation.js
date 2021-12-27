ff0cca4eb9b71ba4da58ed779214d687d71be218
xcv <xcv4716@naver.com>
2021-12-27 16:52:32 +0900

    reser

--- src/page/customer/Reservation.js
+++ src/page/customer/Reservation.js
@@ -4,9 +4,41 @@ import { render } from 'react-dom';
 import Navigation from '../../component/navigation/Navigation';
 import Header from '../../component/header/Header';
 import Footer from '../../component/footer/Footer';
+import { connect } from 'react-redux';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import MegaMenu from '../../component/navigation/Menu';
+import { SERVER_URL } from '../../const/settings';
+const ip = SERVER_URL;
 
 class Reservation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reservation: [],
+        }
+        this.reservationSelect();
+    }
+
+    goLogin = () => {
+        this.props.history.push('/');
+    };
+
+    reservationSelect = () => {
+        fetch(ip + '/reservation/select',
+            {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                }
+            })
+            .then((result) => result.json())
+            .then((result) => {
+                this.setState({ reservation: result })
+                console.log(result)
+            })
+    }
+
     render() {
         return (
             <div className='addCustomer'>
@@ -14,10 +46,32 @@ class Reservation extends Component {
                     <Header />
                     <Navigation goLogin={this.goLogin} />
                     <MegaMenu />
+                    <div className='container'>
+                        Reservation
+                    </div>
                 </header>
+                <div className='footer'>
+                    <Footer />
+                </div>
             </div>
         )
     }
 }
 
-export default Reservation;
+const ReservationStateToProps = (state) => {
+    return {
+        userinfo: state.authentication.userinfo,
+        status: state.authentication.status,
+    };
+};
+
+const ReservationDispatchToProps = (dispatch) => {
+    return {
+        getStatusRequest: () => {
+            return dispatch(getStatusRequest());
+        },
+    };
+};
+
+
+export default connect(ReservationStateToProps, ReservationDispatchToProps)(Reservation);
