515678a4b87c2c1dab995b4c550362a56968b0e2
xcv <xcv4716@naver.com>
2021-12-29 19:08:02 +0900

    reservation page

--- src/page/customer/Reservation.js
+++ src/page/customer/Reservation.js
@@ -1,26 +1,30 @@
 import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import { Container, Row, Col } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import { getStatusRequest } from '../../action/authentication';
 
-import Dropdown from 'react-dropdown';
 
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+// import Dropdown from 'react-dropdown';
 
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+
+// import Table from '@material-ui/core/Table';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableRow from '@material-ui/core/TableRow';
+// import TableCell from '@material-ui/core/TableCell';
+// import axios from 'axios';
 
 import Navigation from '../../component/navigation/Navigation';
 import Header from '../../component/header/Header';
 import Footer from '../../component/footer/Footer';
 import MegaMenu from '../../component/navigation/Menu';
 import moment from 'moment';
-import axios from 'axios';
+
 import { connect } from 'react-redux';
 import 'react-dropdown/style.css';
 
@@ -150,25 +154,75 @@ class Reservation extends Component {
             reservation: [],
 
             customer_name: "홍길동",
-            reservation_date: moment(),
-            reservation_time: moment(),
+            customer_id: "홍길동",
+            isCancel: 1,
+            reserv_date: moment(),
+            reserv_time: moment(),
             exercise_name: "PT기구",
-            is_cancel: false,
-            cancel_comment: "",
+            cancelComment: "",
         },
-            this.reservationSelect();
+            this.reservationSelect()
+        // ,
+        // this.reservationUpdate(),
+        // this.reservationDelete(),
+        // this.reservationInsert()
     }
 
+    componentDidMount() {
+        //컴포넌트 렌더링이 맨 처음 완료된 이후에 바로 세션확인
+        // get cookie by name
+        function getCookie(name) {
+            var value = '; ' + document.cookie;
+            var parts = value.split('; ' + name + '=');
+            if (parts.length == 2) return parts.pop().split(';').shift();
+        }
+
+        // get loginData from cookie
+        let loginData = getCookie('key');
+        // if loginData is undefined, do nothing
+        if (typeof loginData === 'undefined') {
+            this.props.history.push('/');
+            return;
+        }
+
+        // decode base64 & parse json
+        loginData = JSON.parse(atob(loginData));
+        // if not logged in, do nothing
+        if (!loginData.isLoggedIn) {
+            this.props.history.push('/');
+            return;
+        }
+
+        // page refreshed & has a session in cookie,
+        // check whether this cookie is valid or not
+        this.props.getStatusRequest().then(() => {
+            // if session is not valid
+            if (!this.props.status.valid) {
+                // logout the session
+                loginData = {
+                    isLoggedIn: false,
+                    id: '',
+                };
+
+                document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+
+                // and notify
+                alert('Your session is expired, please log in again');
+            } else {
+                this.reservationSelect();
+            }
+        });
+    }
+
+
+
     reservationSelect = () => {
-        fetch(ip + '/reservation/select',
+        fetch(ip + '/reservation/select?fitness_no=' + this.props.userinfo.fitness_no,
             {
                 method: 'GET',
                 headers: {
                     'Content-type': 'application/json',
-                },
-                body: JSON.stringify({
-                    fitness_no: this.props.userinfo.fitness_no
-                })
+                }
             })
             .then((result) => result.json())
             .then((result) => {
@@ -198,6 +252,7 @@ class Reservation extends Component {
             )
                 .then((result) => result.json())
                 .then((result) => {
+                    alert("asdasd")
                     this.setState({
                         reservationInsert: result
                     })
@@ -216,8 +271,8 @@ class Reservation extends Component {
                 },
                 body: JSON.stringify({
                     res_no: this.state.res_no,
-                    date: this.state.date,
-                    time: this.state.time,
+                    reserv_date: this.state.reserv_date,
+                    reserv_time: this.state.reserv_time,
                     exercise_name: this.state.exercise_name,
                     isCancel: this.state.isCancel,
                     cancelComment: this.state.cancelComment
@@ -238,17 +293,22 @@ class Reservation extends Component {
                 this.reservationSelect()
             })
     }
-    handleStartDateChange(date) {
+    handleDateChange(date) {
         this.setState({
             date: date
         })
     }
 
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
 
 
     render() {
-        // console.log(this.state.reservation)
+        console.log(this.state.reservation)
         console.log(this.props.userinfo.fitness_no)
         return (
             <div className='addCustomer'>
@@ -264,34 +324,48 @@ class Reservation extends Component {
                     setCustomer={c => this.setState({ customer: c })}
                 /> */}
                 <Container>
-                    <button type="button" onClick={this.handleOnClick}>예약하기</button>
-                    {/* <Row><Col>회원이름:</Col><Col>{this.state.customer_name}</Col></Row>
-                    <Row><Col>날짜:</Col><Col>{this.state.reservation_date.format("YYYY-MM-DD")}</Col></Row>
-                    <Row><Col>시간:</Col><Col>{this.state.reservation_time.format("hh:mm")}</Col></Row>
+                    <Row><Col>회원이름:</Col><Col>{this.state.customer_name}</Col></Row>
+                    <Row><Col>날짜:</Col><Col>{this.state.reserv_date.format("YYYY-MM-DD")}</Col></Row>
+                    <Row><Col>시간:</Col><Col>{this.state.reserv_time.format("hh:mm")}</Col></Row>
                     <Row><Col>운동이름:</Col><Col>{this.state.exercise_name}</Col></Row>
-                    <Row><Col>취소유무:</Col><Col>{this.state.is_cancel ? "취소됨" : "예약됨"}</Col></Row>
-                    <Row><Col>취소사유:</Col><Col>{this.state.is_cancel && this.state.cancel_comment}</Col></Row> */}
+                    <Row><Col>취소유무:</Col><Col>{this.state.isCancel == 0 ? "취소됨" : "예약됨"}</Col></Row>
+                    <Row><Col>취소사유:</Col><Col>{this.state.cancelComment}</Col></Row>
                 </Container>
                 <Container>
+                    <button type="button" onClick={this.handleOnClick}>예약하기</button>
                     <DatePicker
                         selected={this.state.date}
-                        onChange={this.handleStartDateChange}
+                        onChange={this.handleDateChange}
                         name="date"
                         dateFormat="yyyy-MM-dd"
                         font-size="1.6rem"
                     />
-                    <input id='time' />
+                    {/* <button type={button} id='time' />
+                    <button type={button} id='time' />
+                    <button type={button} id='time' />
+                    <button type={button} id='time' /> */}
                     <TextField
                         id='exercise_name'
+                        value={this.state.exercise_name}
+                        onChange={this.handleChange}
+                        label='운동명'
                     />
                     <TextField
                         id='customer_name'
+                        value={this.state.customer_name}
+                        onChange={this.handleChange}
+                        label='회원이름'
                     />
                     <TextField
                         id='customer_id'
+                        value={this.state.customer_id}
+                        onChange={this.handleChange}
+                        label='회원아이디'
                     />
-
                 </Container>
+                <div className='footer'>
+                    <Footer />
+                </div>
             </div>
         )
     }
