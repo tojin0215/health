a7452127992d47e5b88cadbc290430ce14724a81
seungwoo <seungwoo3859@gmail.com>
2022-05-11 14:35:12 +0900

    로그인 정리

--- src/page/login/Authentication.js
+++ src/page/login/Authentication.js
@@ -59,67 +59,82 @@ class Authentication extends Component {
   render() {
     const inputBoxes = (
       <div>
-        <Form key="loginGroup">
-          <Form.Check
-            inline
-            type="radio"
-            id="loginGroup-1"
-            name="group1"
-            label="사업주"
-          />
-          <Form.Check
-            inline
-            type="radio"
-            id="loginGroup-2"
-            name="group1"
-            label="강사"
-          />
-          <Form.Check
-            inline
-            type="radio"
-            id="loginGroup-3"
-            name="group1"
-            label="회원"
-          />
-          <Form.Group className="mb-3" controlId="formBasicId">
-            <Form.Label>아이디</Form.Label>
-            <Form.Control
-              name="id"
-              type="text"
-              className="validate"
-              placeholder="Enter id"
-              onChange={this.handleChange}
-              value={this.state.id}
+        <Form key="loginGroup" className="pt-4">
+          <div className="member_wrap mt-2">
+            <Form.Check
+              inline
+              type="radio"
+              id="loginGroup-1"
+              name="group1"
+              checked="checked"
+              label="사업주"
             />
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              name="password"
-              type="password"
-              className="validate"
-              onChange={this.handleChange}
-              value={this.state.password}
-              onKeyPress={this.handleKeyPress}
-              placeholder="Password"
+            <Form.Check
+              inline
+              type="radio"
+              id="loginGroup-2"
+              name="group1"
+              label="강사"
             />
-          </Form.Group>
-          <Button onClick={this.handleLogin} className="" variant="primary">
-            LOGIN
-          </Button>
-          <Button className="" variant="secondary" href="/register">
-            회원가입
-          </Button>
+            <Form.Check
+              inline
+              type="radio"
+              id="loginGroup-3"
+              name="group1"
+              label="회원"
+            />
+          </div>
+          <div className="id_pw_wrap mt-4">
+            <Form.Group className="mb-3" controlId="formBasicId">
+              <Form.Control
+                name="id"
+                type="text"
+                className="validate"
+                placeholder="아이디"
+                onChange={this.handleChange}
+                value={this.state.id}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                name="password"
+                type="password"
+                className="validate"
+                onChange={this.handleChange}
+                value={this.state.password}
+                onKeyPress={this.handleKeyPress}
+                placeholder="비밀번호"
+              />
+            </Form.Group>
+          </div>
+          <div className="btn_login_wrap">
+            <Button
+              onClick={this.handleLogin}
+              className="w-100"
+              variant="primary"
+            >
+              로그인
+            </Button>
+          </div>
         </Form>
       </div>
     );
 
     const loginView = (
-      <div>
+      <div className="login_wrap">
         <Card className="text-center">
           <Card.Body>{inputBoxes}</Card.Body>
         </Card>
+        <div className="btn_login_wrap justify-content-center text-center">
+          <Button
+            className="border-0 text-secondary"
+            variant="outline-light"
+            href="/register"
+          >
+            회원가입
+          </Button>
+        </div>
       </div>
     );
 
@@ -132,9 +147,10 @@ class Authentication extends Component {
     //         </div>
     //     </div>
     // );
+
     return (
       <div className="container auth">
-        <div className="card">{this.props.mode ? loginView : registerView}</div>
+        <div className="">{this.props.mode ? loginView : registerView}</div>
       </div>
     );
   }
--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -3,7 +3,6 @@ import Authentication from '../login/Authentication';
 import Navigation from '../../component/navigation/Navigation';
 import { Slide } from 'react-slideshow-image';
 
-
 import Header from '../../component/header/Header';
 import Footer from '../../component/footer/Footer';
 import { connect } from 'react-redux';
@@ -14,72 +13,72 @@ import '../../styles/login/Login.css';
 import 'react-slideshow-image/dist/styles.css';
 
 const fadeProperties = {
-	duration: 2000,
-	canSwipe: false,
+  duration: 2000,
+  canSwipe: false,
 };
 
 class Login extends Component {
-	handleLogin = (id, pw) => {
-		return this.props.loginRequest(id, pw).then(() => {
-			if (this.props.status === 'SUCCESS') {
-				// create session data
-				let loginData = {
-					isLoggedIn: true,
-					id: id,
-				};
-				document.cookie = 'key=' + btoa(JSON.stringify(loginData));
-				alert(id + '님 반갑습니다.');
-				this.props.history.push('/home');
-				return true;
-			} else if (this.props.status === 'PERMITWAITING') {
-				alert('승인 대기 중입니다.');
-				return false;
-			} else {
-				alert('ID나 비밀번호를 확인해주세요.');
-				return false;
-			}
-		});
-	};
+  handleLogin = (id, pw) => {
+    return this.props.loginRequest(id, pw).then(() => {
+      if (this.props.status === 'SUCCESS') {
+        // create session data
+        let loginData = {
+          isLoggedIn: true,
+          id: id,
+        };
+        document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+        alert(id + '님 반갑습니다.');
+        this.props.history.push('/home');
+        return true;
+      } else if (this.props.status === 'PERMITWAITING') {
+        alert('승인 대기 중입니다.');
+        return false;
+      } else {
+        alert('ID나 비밀번호를 확인해주세요.');
+        return false;
+      }
+    });
+  };
 
-	render() {
-		return (
-			<div className='wrap loginWrap'>
-				{/* <div className='header'>
+  render() {
+    return (
+      <div className="">
+        {/* <div className='header'>
 					<Header />
 					<Navigation />
 				</div> */}
-				<div className='localNavigation'>
-					<div className='container'>
-						<h2>
-							<div className='parallelogram'></div>
-							로그인
-							<span>.</span>
-						</h2>
-					</div>
-				</div>
-				<div className='container'>
-					<Authentication mode={true} onLogin={this.handleLogin} />
-				</div>
-				<div className='footer'>
-					<Footer />
-				</div>
-			</div>
-		);
-	}
+        <div className="localNavigation">
+          <div className="container">
+            <h2>
+              <div className="parallelogram"></div>
+              로그인
+              <span>.</span>
+            </h2>
+          </div>
+        </div>
+        <div className="container">
+          <Authentication mode={true} onLogin={this.handleLogin} />
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		status: state.authentication.login.status,
-	};
+  return {
+    status: state.authentication.login.status,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		loginRequest: (id, pw) => {
-			return dispatch(loginRequest(id, pw));
-		},
-	};
+  return {
+    loginRequest: (id, pw) => {
+      return dispatch(loginRequest(id, pw));
+    },
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
