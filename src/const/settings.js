bcd5d3d24f6dab05ed88b903f0c4f6d39eccd1e1
developerTojin <mb8281@gmail.com>
2022-07-04 10:11:14 +0900

    loacal

--- src/const/settings.js
+++ src/const/settings.js
@@ -1,6 +1,6 @@
 // export const SERVER_URL = 'https://ohmygym.kr/api';
 // export const SERVER_URL = '13.124.141.28:3002';
-export const SERVER_URL = 'http://localhost:3000';
+// export const SERVER_URL = 'http://localhost:3000';
 // export const SERVER_URL = '/api';
-// export const SERVER_URL = '';
+export const SERVER_URL = '';
 //aws  "proxy": "http://13.124.141.28:3002/",
