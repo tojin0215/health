bf7d341964fdd4e3397203791f6cb3f4197db673
developerTojin <mb8281@gmail.com>
2022-07-05 13:45:46 +0900

    https

--- src/const/settings.js
+++ src/const/settings.js
@@ -1,4 +1,3 @@
-// export const SERVER_URL = 'https://ohmygym.kr/api';
 // export const SERVER_URL = '13.124.141.28:3002';
 // export const SERVER_URL = 'http://localhost:3000';
 export const SERVER_URL = '/api';
