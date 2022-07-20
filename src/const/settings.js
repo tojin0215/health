9bbbe4e66ba0b46055f9385ea5b50535a659b0a4
developerTojin <mb8281@gmail.com>
2022-07-20 09:35:25 +0900

    asd

--- src/const/settings.js
+++ src/const/settings.js
@@ -1,7 +1,7 @@
 // export const SERVER_URL = '13.124.141.28:3002';
 // export const SERVER_URL = 'http://localhost:3000';
-// export const SERVER_URL = '/api';
-export const SERVER_URL = '';
+export const SERVER_URL = 'api';
+// export const SERVER_URL = '';
 //aws  "proxy": "http://13.124.141.28:3002/",
 //divvy "proxy": "https://divvy.fit/",
 //local "proxy": "http://localhost:3002",
