6b1bb8da0605a40b010adf662214316c0d198141
xcv <xcv4716@naver.com>
2022-08-30 17:40:19 +0900

    build1

--- src/const/settings.js
+++ src/const/settings.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = '';
-// export const SERVER_URL = 'api';
+// export const SERVER_URL = '';
+export const SERVER_URL = 'api';
 // export const SERVER_URL = 'http://localhost:3000';
 // export const SERVER_URL = '13.124.141.28:3002';
 //aws  "proxy": "http://13.124.141.28:3002/",
